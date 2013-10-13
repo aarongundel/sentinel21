6314a4c2f94818220ac5db5ffe788f38175da227
aaron.gundel <aaron.gundel@gmail.com>
2013-10-12 21:31:20 -0700

    Removed the request module, switch to tumblr npm module

--- routes/api/posts/show.js
+++ routes/api/posts/show.js
@@ -1,4 +1,3 @@
-request = require('request');
 var Blog = require('tumblr').Blog
 
 module.exports = function(req, response, abc){
@@ -15,25 +14,4 @@ module.exports = function(req, response, abc){
             response.send(data.posts);
         }
     });
-/*
-    var options = {
-        uri: 'https://api.tumblr.com/v2/blog/aarongundel.tumblr.com/posts?id=' + req.params.id + '&api_key=' + process.env.TUMBLR_KEY,
-        method: 'GET',
-        json: true
-    }
-    request(options, function(err, res, data){
-        if(err){
-            console.log(err);
-            response.send({error: 'A lookup error has occurred.'});
-        } else {
-            if(data.response.posts.length)
-            {
-                response.send(data.response.posts[0]);
-            } else
-            {
-                response.send({});
-            }
-        }
-    });*/
-    
 };
